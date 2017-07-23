08ad91956a25bc091d03ec80eb5fe8af3569be96
andreataglia <drew.taglia@gmail.com>
2017-07-23 17:24:52 +0200

    nodemailer sistemato. turned into nodejs app

---
+++ public/js/map.markers.js
@@ -0,0 +1,17 @@
+//List of map markers
+var map_markers = [
+	{
+		"title": "Cerimonia",
+		"latitude":45.7974375,
+		"longitude":8.7571673,
+		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Parrocchia Natività di Maria Vergine <br> Piazza Giovanni XXIII 3, Azzate, Varese"
+	},
+	{
+		"title": "Ricevimento",
+		"latitude":45.9193907,
+		"longitude":8.5536933,
+		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Villa Rusconi Clerici <br> Via Vittorio Veneto 16, Pallanza Verbania, Verbania"
+	}
+];
